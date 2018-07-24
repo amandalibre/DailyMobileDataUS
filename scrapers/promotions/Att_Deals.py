3386687c0e6f330051f408dddd709ec91a92e2df
amandalibre <amandalibre421@gmail.com>
2018-07-24 12:13:44 -0400

    add legal terms to att_deals

--- scrapers/promotions/Att_Deals.py
+++ scrapers/promotions/Att_Deals.py
@@ -18,14 +18,13 @@ def att_scrape_deals_page():
     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
     driver.implicitly_wait(6)
 
-
     # go to website
     driver.get('https://www.att.com/deals/')
     time.sleep(10)
     html = driver.page_source
     soup = BeautifulSoup(html, "html.parser")
 
-    # screen shot experiment
+    # screen shot
     today = str(datetime.datetime.today().date())
     fullpage_screenshot(driver, r'C:\Users\Amanda Friedman\PycharmProjects\DailyPromotionsAndPricing\Screenshots\att_deals_' + today + '.png')
 
@@ -43,25 +42,48 @@ def att_scrape_deals_page():
     for slideshow in soup.findAll('div', class_='content-wrapper'):
         deals_page_promo = slideshow.text.strip().replace('\n', '')
         scraped_promotion.promo_text = deals_page_promo
-        print(scraped_promotion.provider, scraped_promotion.device_name, scraped_promotion.device_storage,
-              scraped_promotion.promo_location, scraped_promotion.promo_text, scraped_promotion.url,
-              scraped_promotion.date, scraped_promotion.time)
         add_scraped_promotions_to_database(scraped_promotion.provider, scraped_promotion.device_name,
                                            scraped_promotion.device_storage, scraped_promotion.promo_location,
                                            scraped_promotion.promo_text, scraped_promotion.url, scraped_promotion.date,
                                            scraped_promotion.time)
+        item_details = slideshow.find("div", {"class": "legal"})
+        try:
+            legal_link = "https://www.att.com/" + item_details.a["data-ajaxdata"]
+            driver.get(legal_link)
+            time.sleep(2)
+            html = driver.page_source
+            legal_soup = BeautifulSoup(html, "html.parser")
+            for legal_terms in legal_soup.body.findAll("div")[1:]:
+                scraped_promotion.promo_text = "LEGAL TERMS: " + legal_terms.text.strip()
+                add_scraped_promotions_to_database(scraped_promotion.provider, scraped_promotion.device_name,
+                                                   scraped_promotion.device_storage, scraped_promotion.promo_location,
+                                                   scraped_promotion.promo_text, scraped_promotion.url, scraped_promotion.date,
+                                                   scraped_promotion.time)
+        except KeyError:
+            pass
 
     for div in soup.findAll('div', class_='item-wrapper'):
         deals_page_promo = div.text.strip().replace('\n', '')
         scraped_promotion.promo_text = deals_page_promo
-        print(scraped_promotion.provider, scraped_promotion.device_name, scraped_promotion.device_storage,
-              scraped_promotion.promo_location, scraped_promotion.promo_text, scraped_promotion.url,
-              scraped_promotion.date, scraped_promotion.time)
         add_scraped_promotions_to_database(scraped_promotion.provider, scraped_promotion.device_name,
                                            scraped_promotion.device_storage, scraped_promotion.promo_location,
                                            scraped_promotion.promo_text, scraped_promotion.url, scraped_promotion.date,
                                            scraped_promotion.time)
+        item_details = div.find("div", {"class": "legal"})
+        try:
+            legal_link = "https://www.att.com/" + item_details.a["data-ajaxdata"]
+            driver.get(legal_link)
+            time.sleep(2)
+            html = driver.page_source
+            legal_soup = BeautifulSoup(html, "html.parser")
+            for legal_terms in legal_soup.body.findAll("div")[1:]:
+                scraped_promotion.promo_text = "LEGAL TERMS: " + legal_terms.text.strip()
+                add_scraped_promotions_to_database(scraped_promotion.provider, scraped_promotion.device_name,
+                                                   scraped_promotion.device_storage, scraped_promotion.promo_location,
+                                                   scraped_promotion.promo_text, scraped_promotion.url, scraped_promotion.date,
+                                                   scraped_promotion.time)
+        except AttributeError:
+            pass
 
     driver.quit()
-
 
